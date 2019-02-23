c444ceba1bd2c528f5005990846b36ef659f9a93
checho <sergio.baron.2019@upb.edu.co>
2019-02-22 22:12:56 -0500

    mi primera modificacion

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ doble= numa*2
 
 print ("el producto de los dos numeros es " + str(prod))
 print ("el doble de " + str(numa) + " es " + str(doble))
+
+import math
+numb= int(input("ditite el numero entero b"))
+numd= int(input("ditite el numero entero d"))
+
+cuad= pow(numb,2)
+raiz= math.sqrt(numd) 
+
+print("el cuadrado de " + str(numb) + " es " + str(cuad))
+print("la raiz cuadrada de " + str(numd) + " es " + str(raiz))
