012b1b9ea34e954144d5d98ab3f2f6b75b08a9b7
checho <sergio.baron.2019@upb.edu.co>
2019-02-23 06:51:23 -0500

    mas correcciones de ortografía

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-print("Este programa calcula el producto de dos nueros enteros y el doble del primer numero.")
+print("Este programa calcula el producto de dos núeros enteros y el doble del primer número.")
 
-numa= int(input("Digite el numero entero a"))
-numc= int(input("Digite el numero entero c"))
+numa= int(input("Digite el número entero a"))
+numc= int(input("Digite el número entero c"))
 
 prod= numa*numc
 doble= numa*2
 
-print ("El producto de los dos numeros es " + str(prod))
+print ("El producto de los dos números es " + str(prod))
 
 print ("El doble de " + str(numa) + " es " + str(doble))
 
-print("Este programa calcula el cuadrado del primer numero y la raiz cuadrada de un segundo numero.") 
+print("Este programa calcula el cuadrado del primer número y la raíz cuadrada de un segundo número.") 
 
 import math
-numb= int(input("Digite el numero entero b"))
-numd= int(input("Digite el numero entero d"))
+numb= int(input("Digite el número entero b"))
+numd= int(input("Digite el número entero d"))
 
 cuad= pow(numb,2)
 raiz= math.sqrt(numd) 
 
 print("El cuadrado de " + str(numb) + " es " + str(cuad))
-print("La raiz cuadrada de " + str(numd) + " es " + str(raiz))
+print("La raíz cuadrada de " + str(numd) + " es " + str(raiz))
 
 print("Este programa permite calcular la solución de una ecuación cuadrática de tipo    ax2 + bx + c. ")
 
@@ -31,7 +31,7 @@ c= int(input("Ingrese el valor de c"))
 dentro= b*b-(4*a*c)
 if(dentro==0):
 	x1= -b/(2*a)
-	print("Existe una riz doble de valor: " + str(x1))
+	print("Existe una ríz doble de valor: " + str(x1))
 elif(dentro>0):
 	dentro= math.sqrt(dentro)
 	x1= (-b + dentro)/(2*a)
@@ -40,5 +40,4 @@ elif(dentro>0):
 else:
 	x1= -b/(2*a)
 	raizc= math.sqrt((-dentro/(2*a)))
-	print("Las raices imaginarias: " + str(x1) + " + " + str(raizc) + "i  y " + str(x1) + " - " + str(raizc) +"i"
-
+	print("Las raíces imaginarias: " + str(x1) + " + " + str(raizc) + "i  y " + str(x1) + " - " + str(raizc) +"i")
