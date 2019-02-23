e8ff887f3ac04749663d8212dfcb81dc2f874d92
checho <sergio.baron.2019@upb.edu.co>
2019-02-22 21:54:06 -0500

    Mi primer commit

---
+++ main.py
@@ -0,0 +1,8 @@
+numa= int(input("ditite el numero entero a"))
+numc= int(input("ditite el numero entero c"))
+
+prod= numa*numc
+doble= numa*2
+
+print ("el producto de los dos numeros es " + str(prod))
+print ("el doble de " + str(numa) + " es " + str(doble))
